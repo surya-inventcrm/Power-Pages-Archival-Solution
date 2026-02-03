245c970115e3d09e24c846454720fc30d52cb1d7
inventcrm <v_suryait@live.com>
2026-02-03 22:59:15 +1100

    Update mark-for-archive.js

--- scripts/mark-for-archive.js
+++ scripts/mark-for-archive.js
@@ -5,3 +5,17 @@ function markForArchive(recordId) {
     body: JSON.stringify({ cr_markforarchive: true })
   });
 }
+
+
+Sample:
+fetch("/_api/pp_requests?$filter=createdon lt 2024-01-01")
+  .then(r => r.json())
+  .then(data => {
+    data.value.forEach(r => {
+      fetch(`/_api/pp_requests(${r.pp_requestid})`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ pp_isarchived: true })
+      });
+    });
+  });
